bc9c978a1490821b4098276ab7d926f7876b35b8
YaroslavZhurbilo <4872439@gmail.com>
2017-10-10 16:29:14 +0300

    Added tab bar with items: overview, read, options with view controllers

---
+++ QuitSmokingTogether/AlertVC.swift
@@ -0,0 +1,30 @@
+//
+//  AlertVC.swift
+//  QuitSmokingTogether
+//
+//  Created by Yaroslav Zhurbilo on 10.10.17.
+//  Copyright © 2017 Yaroslav Zhurbilo. All rights reserved.
+//
+
+import UIKit
+
+class AlertVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
