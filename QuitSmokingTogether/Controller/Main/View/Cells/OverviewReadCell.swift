bfe910f40d74f995a6fdfa6a7ac8c01e02a1121d
YaroslavZhurbilo <4872439@gmail.com>
2018-04-19 05:57:26 +0300

    NEW: Added Russian Book Cover

--- QuitSmokingTogether/Controller/Main/View/Cells/OverviewReadCell.swift
+++ QuitSmokingTogether/Controller/Main/View/Cells/OverviewReadCell.swift
@@ -10,15 +10,26 @@ import UIKit
 
 class OverviewReadCell: UITableViewCell {
 
+    @IBOutlet weak var bookCoverImage: UIImageView!
     @IBOutlet weak var bookTitleLabel: UILabel!
     @IBOutlet weak var bookTextLabel: UILabel!
     @IBOutlet weak var continueReadingView: UILabel!
     
     func updateCell() {
+        bookCoverImage.image = getLocalizedCover()
         bookTitleLabel.text = UserDefaultsManager().chapterInText
         bookTextLabel.text = UserDefaultsManager().firstParagraphInText
         continueReadingView.setRadius(10, withWidth: 1, andColor: UIColor.clear)
         continueReadingView.text = "button_continue_reading".localized()
+    }
+    
+    func getLocalizedCover() -> UIImage? {
+        var imageName = "book-cover-"
+        switch LanguageManager.shared.currentLanguage {
+        case .russian: imageName += "ru"
+        default: imageName += "en"
+        }
+        return UIImage(named: imageName)
     }
 
 }
